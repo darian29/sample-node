d6b4a117db2454bab6a6f8eb3367146ec8a52faf
Darian Mendez <darianmendez@hotmail.com>
2024-07-23 21:10:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/api', (req,res) => res.json("Welcome to my API"))
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
